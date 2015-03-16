8dff69557b14a62c22d3e0beb40c1bca2902519d
Afifhandika <afifhandika@yahoo.co.id>
2015-03-16 21:35:56 +0700

    Coba

---
+++ Tugas 1/Coba.java
@@ -0,0 +1,16 @@
+public class Coba
+ {
+	public static void main(String[] args)
+	{
+	 int x=30;
+
+	 if(x <20)
+	 {
+	 	System.out.print("ini adalah statemen if");
+	 }
+	 else
+	{
+		System.out.print("ini adalah statemen else");
+	}
+	}
+}
