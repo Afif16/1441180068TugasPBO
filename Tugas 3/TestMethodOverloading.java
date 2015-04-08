2f6896db2f7fe5caf03ed6ac616038eb97c638cf
Afifhandika <afifhandika@yahoo.co.id>
2015-04-08 15:32:59 +0700

    tugas

--- Tugas 3/TestMethodOverloading.java
+++ Tugas 3/TestMethodOverloading.java
@@ -8,7 +8,7 @@ public class TestMethodOverloading {
 	}	
 
 	public static int average(int n1, int n2, int n3) {
-		return (n1+n2+n3)/2;
+		return (n1+n2+n3)/3;
 	}
 
 	public static void main(String[] args) {
