433aa7d9ea2c55b62dcf7823bfe780f2bc241cd7
Afifhandika <afifhandika@yahoo.co.id>
2015-04-09 23:21:27 +0700

    Circle

---
+++ Tugas 3/circle.java
@@ -0,0 +1,27 @@
+public class circle {
+	double radius;
+	String color;
+
+	public circle (){
+
+		radius = 2.0;
+		color = "pink";
+	}
+	public  circle (double r){
+		radius = r;
+		color = "pink";
+	}
+	public  circle (double r, String c){
+		radius = r;
+		color = c;
+	}
+	public double getRadius () {
+		return radius;
+	}
+	public  String getColor(){
+		return color;
+	}
+	public  double getArea (){
+		return radius*radius*Math.PI;
+	}	
+}
